4bf2de289b5b61055ed67bfbf9b4909c52a2131f
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-08-25 18:09:05 +0200

    First version (#1)
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ src/main/java/org/gridsuite/merge/notification/server/MergeNotificationWebSocketHandler.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.merge.notification.server;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.UncheckedIOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+
+/**
+ * A WebSocketHandler that sends messages from a broker to websockets opened by clients, interleaving with pings to keep connections open.
+ *
+ * Spring Cloud Stream gets the consumeNotification bean and calls it with the
+ * flux from the broker. We call publish and connect to subscribe immediately to the flux
+ * and multicast the messages to all connected websockets and to discard the messages when
+ * no websockets are connected.
+ *
+ * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
+ * @author Jon Harper <jon.harper at rte-france.com>
+ */
+@Component
+public class MergeNotificationWebSocketHandler implements WebSocketHandler {
+
+    private static final String CATEGORY_BROKER_INPUT = MergeNotificationWebSocketHandler.class.getName() + ".input-broker-messages";
+    private static final String CATEGORY_WS_OUTPUT = MergeNotificationWebSocketHandler.class.getName() + ".output-websocket-messages";
+
+    private static final String QUERY_PROCESS = "process";
+    private static final String QUERY_DATE = "date";
+
+    private static final String HEADER_TSO = "tso";
+    private static final String HEADER_TYPE = "type";
+    private static final String HEADER_DATE = "date";
+    private static final String HEADER_PROCESS = "process";
+
+    private ObjectMapper jacksonObjectMapper;
+
+    private int heartbeatInterval;
+
+    public MergeNotificationWebSocketHandler(ObjectMapper jacksonObjectMapper, @Value("${notification.websocket.heartbeat.interval:30}") int heartbeatInterval) {
+        this.jacksonObjectMapper = jacksonObjectMapper;
+        this.heartbeatInterval = heartbeatInterval;
+    }
+
+    Flux<Message<String>> flux;
+
+    @Bean
+    public Consumer<Flux<Message<String>>> consumeNotification() {
+        return f -> {
+            ConnectableFlux<Message<String>> c = f.log(CATEGORY_BROKER_INPUT, Level.FINE).publish();
+            this.flux = c;
+            c.connect();
+        };
+    }
+
+    /**
+     * map from the broker flux to the filtered flux for one websocket client, extracting only relevant fields.
+     */
+    private Flux<WebSocketMessage> notificationFlux(WebSocketSession webSocketSession, String process) {
+        return flux.transform(f -> {
+            Flux<Message<String>> res = f;
+            if (process != null) {
+                res = res.filter(m -> process.equals(m.getHeaders().get(HEADER_PROCESS)));
+            }
+            return res;
+        }).map(m -> {
+            try {
+                Map<String, Object> submap = Map.of(
+                        "payload", m.getPayload(),
+                        "headers", Map.of(
+                                HEADER_TSO, m.getHeaders().get(HEADER_TSO),
+                                HEADER_TYPE, m.getHeaders().get(HEADER_TYPE),
+                                HEADER_PROCESS, m.getHeaders().get(HEADER_PROCESS),
+                                HEADER_DATE, m.getHeaders().get(HEADER_DATE)));
+                return jacksonObjectMapper.writeValueAsString(submap);
+            } catch (JsonProcessingException e) {
+                throw new UncheckedIOException(e);
+            }
+        }).map(webSocketSession::textMessage);
+    }
+
+    /**
+     * A heartbeat flux sending websockets pings
+     */
+    private Flux<WebSocketMessage> heartbeatFlux(WebSocketSession webSocketSession) {
+        return Flux.interval(Duration.ofSeconds(heartbeatInterval)).map(n -> webSocketSession
+                .pingMessage(dbf -> dbf.wrap((webSocketSession.getId() + "-" + n).getBytes(StandardCharsets.UTF_8))));
+    }
+
+    @Override
+    public Mono<Void> handle(WebSocketSession webSocketSession) {
+        URI uri = webSocketSession.getHandshakeInfo().getUri();
+        MultiValueMap<String, String> parameters = UriComponentsBuilder.fromUri(uri).build().getQueryParams();
+        String process = parameters.getFirst(QUERY_PROCESS);
+        return webSocketSession
+                .send(
+                        notificationFlux(webSocketSession, process)
+                        .mergeWith(heartbeatFlux(webSocketSession))
+                        .log(CATEGORY_WS_OUTPUT, Level.FINE))
+                .and(webSocketSession.receive());
+    }
+}
---
+++ src/test/java/org/gridsuite/merge/notification/server/MergeNotificationWebSocketHandlerTest.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.merge.notification.server;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.web.reactive.socket.HandshakeInfo;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+
+import java.io.UncheckedIOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
+ * @author Jon Harper <jon.harper at rte-france.com>
+ */
+public class MergeNotificationWebSocketHandlerTest {
+
+    private ObjectMapper objectMapper;
+    private WebSocketSession ws;
+    private HandshakeInfo handshakeinfo;
+    private UriComponentsBuilder uriComponentBuilder;
+
+    @Before
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        var dataBufferFactory = new DefaultDataBufferFactory();
+
+        ws = Mockito.mock(WebSocketSession.class);
+        handshakeinfo = Mockito.mock(HandshakeInfo.class);
+        uriComponentBuilder = UriComponentsBuilder.fromUriString("http://localhost:1234/notify");
+
+        when(handshakeinfo.getUri()).thenReturn(uriComponentBuilder.build().toUri());
+        when(ws.getHandshakeInfo()).thenReturn(handshakeinfo);
+        when(ws.receive()).thenReturn(Flux.empty());
+        when(ws.send(any())).thenReturn(Mono.empty());
+        when(ws.textMessage(any())).thenAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+            String str = (String) args[0];
+            return new WebSocketMessage(WebSocketMessage.Type.TEXT, dataBufferFactory.wrap(str.getBytes()));
+        });
+        when(ws.pingMessage(any())).thenAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+            Function<DataBufferFactory, DataBuffer> f = (Function<DataBufferFactory, DataBuffer>) args[0];
+            return new WebSocketMessage(WebSocketMessage.Type.PING, f.apply(dataBufferFactory));
+        });
+        when(ws.getId()).thenReturn("testsession");
+
+    }
+
+    private void withFilters(String process) {
+        var notificationWebSocketHandler = new MergeNotificationWebSocketHandler(objectMapper, Integer.MAX_VALUE);
+
+        if (process != null) {
+            uriComponentBuilder.queryParam("process", process);
+        }
+
+        var atomicRef = new AtomicReference<FluxSink<Message<String>>>();
+        var flux = Flux.create(atomicRef::set);
+        notificationWebSocketHandler.consumeNotification().accept(flux);
+        var sink = atomicRef.get();
+
+        notificationWebSocketHandler.handle(ws);
+
+        List<Map<String, Object>> refMessages = Arrays.asList(
+                Map.of("process", "SWE", "date", "2020-07-01 02:30:00.000000+0000", "tso", "RTE", "type", "TEST"),
+                Map.of("process", "SWE", "date", "2020-07-01 03:30:00.000000+0000", "tso", "RTE", "type", "TEST"),
+                Map.of("process", "CORE", "date", "2020-07-01 04:30:00.000000+0000", "tso", "RTE", "type", "TEST")
+        );
+
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Flux<WebSocketMessage>> argument = ArgumentCaptor.forClass(Flux.class);
+        verify(ws).send(argument.capture());
+        argument.getValue().map(WebSocketMessage::getPayloadAsText).collectList().subscribe(list -> {
+            List<Map<String, Object>> expected = refMessages.stream().filter(headers -> {
+                String processValue = (String) headers.get("process");
+                return process == null || process.equals(processValue);
+            }).collect(Collectors.toList());
+            List<Map<String, Object>> actual = list.stream().map(t -> {
+                try {
+                    var deserializedHeaders = ((Map<String, Map<String, Object>>) objectMapper.readValue(t, Map.class))
+                            .get("headers");
+                    return Map.of("process", deserializedHeaders.get("process"), "date",
+                            deserializedHeaders.get("date"));
+                } catch (JsonProcessingException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }).collect(Collectors.toList());
+            assertEquals(expected, actual);
+        });
+
+        refMessages.stream().map(headers -> new GenericMessage<>("", headers)).forEach(sink::next);
+        sink.complete();
+    }
+
+    @Test
+    public void testWithoutFilter() {
+        withFilters(null);
+    }
+
+    @Test
+    public void testProcessFilter() {
+        withFilters("SWE");
+    }
+
+    @Test
+    public void testHeartbeat() {
+        var notificationWebSocketHandler = new MergeNotificationWebSocketHandler(null, 1);
+
+        when(handshakeinfo.getUri()).thenReturn(uriComponentBuilder.build().toUri());
+        var flux = Flux.<Message<String>>empty();
+        notificationWebSocketHandler.consumeNotification().accept(flux);
+        notificationWebSocketHandler.handle(ws);
+
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Flux<WebSocketMessage>> argument = ArgumentCaptor.forClass(Flux.class);
+        verify(ws).send(argument.capture());
+        assertEquals("testsession-0", argument.getValue().blockFirst(Duration.ofSeconds(10)).getPayloadAsText());
+    }
+
+}
